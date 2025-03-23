fe0d4a669035320476f18aeb54ead3546e9b1345
bonnmh <bonnmh@nami.trade>
2025-03-23 18:28:16 +0700

    [bonnmh] - [23/03] init

--- lynx.config.ts
+++ lynx.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@lynx-js/rspeedy'
 
 import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
-import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin-canary';
 
 export default defineConfig({
   plugins: [
